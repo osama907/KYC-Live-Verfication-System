e7d123766d884813879545d10ee8e93c8fc7de55
User <user@example.com>
2026-01-27 09:37:16 +0500

    Update code changes

--- main1.py
+++ main1.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500", "http://localhost:5500"],
+    allow_origins=["*"],  # Allow all for testing, or put your specific frontend URL here
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
